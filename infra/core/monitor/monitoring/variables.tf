03549aa90c2a1cc12d7af16f3f011453879df948
Jashanpreet Magar <jashanpreet.magar@qualyfi.co.uk>
2024-03-11 20:45:19 +0000

    Added monitor module

---
+++ infra/core/monitor/monitoring/variables.tf
@@ -0,0 +1,27 @@
+variable "log_analytics_name" {
+  type = string
+  description = "The name of the log analytics"
+}
+
+variable "location" {
+  type = string
+  description = "The location of the rg"
+}
+
+variable "resource_group_name" {
+  type = string
+  description = "The rg name"
+}
+
+variable "application_insights_name" {
+  type = string
+  description = "The name of app insights"
+  default = ""
+}
+
+variable "application_insights_dashboard_name" {
+  type = string
+  description = "The name of app insights dashboard"
+  default = ""
+}
+
