7b3ff90fdad6cda15e37c603d00cf02766ce9384
Jashanpreet Magar <jashanpreet.magar@qualyfi.co.uk>
2024-03-11 15:55:02 +0000

    Created app service plan

---
+++ infra/core/host/appserviceplan/variables.tf
@@ -0,0 +1,29 @@
+variable "name" {
+  type = string
+  description = "The name of the app service plan"
+}
+
+variable "location" {
+  type = string
+  description = "The location of the app service plan"
+}
+
+variable "resource_group_name" {
+  type = string
+  description = "The rg name"
+}
+variable "kind" {
+  type = string
+  description = "The kind of app service"
+}
+variable "sku" {
+  type = object({
+    tier = string
+    size = string
+  })
+  description = "The sku fro apps ervice"
+}
+variable "reserved" {
+  type = string
+}
+
