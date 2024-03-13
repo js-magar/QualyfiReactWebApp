eba46938b00ea230de4979e8e1b859d93aa0c65c
Jashanpreet Magar <jashanpreet.magar@qualyfi.co.uk>
2024-03-13 16:34:58 +0000

    Changed kv_policy name

--- infra/core/security/keyvault-access/main.tf
+++ infra/core/security/keyvault-access/main.tf
@@ -4,13 +4,13 @@ data "azurerm_key_vault" "key_vault" {
   name                        = var.key_vault_name
   resource_group_name         = var.resource_group_name
 }
-resource "azurerm_key_vault_access_policy" "key_vault_access_policy" {
+resource "azurerm_key_vault_access_policy_1" "key_vault_access_policy" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = var.principal_id
   secret_permissions = var.permissions.secrets
 }
-resource "azurerm_key_vault_access_policy" "key_vault_access_policy" {
+resource "azurerm_key_vault_access_policy_2" "key_vault_access_policy" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
