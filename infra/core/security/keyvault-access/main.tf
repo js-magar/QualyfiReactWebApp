9af50e9fbf7f7ce4f9910358b9289ab220be869f
Jashanpreet Magar <jashanpreet.magar@qualyfi.co.uk>
2024-03-13 16:24:26 +0000

    Changed KV Access

--- infra/core/security/keyvault-access/main.tf
+++ infra/core/security/keyvault-access/main.tf
@@ -10,3 +10,9 @@ resource "azurerm_key_vault_access_policy" "key_vault_access_policy" {
   object_id    = var.principal_id
   secret_permissions = var.permissions.secrets
 }
+resource "azurerm_key_vault_access_policy" "key_vault_access_policy" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+  secret_permissions = var.permissions.secrets
+}
