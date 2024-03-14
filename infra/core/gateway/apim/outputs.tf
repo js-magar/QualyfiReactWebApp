af9e2fece508352521c39f5c0afaf7384aef003a
Jashanpreet Magar <jashanpreet.magar@qualyfi.co.uk>
2024-03-14 12:49:28 +0000

    Fixed path error in apim api

--- infra/core/gateway/apim/outputs.tf
+++ infra/core/gateway/apim/outputs.tf
@@ -3,6 +3,6 @@ output "apim_service_name" {
     value = "${azurerm_api_management.apim.name}"
 }
 output "api_management_logger_id" {
-  value = "${azurerm_api_management_logger.logger.id}"
+  value = "${azurerm_api_management_logger.apim_logger.id}"
 }
 
